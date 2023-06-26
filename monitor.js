b650767e37cb636ae3391775aeb1f04a2c91cee9
Samuel Watson <planetbridging@gmail.com>
2023-06-26 11:45:28 +1000

    neat just needed to detect and pass through post

--- monitor.js
+++ monitor.js
@@ -371,6 +371,15 @@ class objMonitor {
           //console.log(this.websitePaths);
 
           this.updateUUIDForWebsiteMap();
+
+          if (req.method == "POST" && req.body) {
+            let bodyData = JSON.stringify(req.body);
+            // incase if content-type is application/x-www-form-urlencoded -> we need to change to application/json
+            proxyReq.setHeader("Content-Type", "application/json");
+            proxyReq.setHeader("Content-Length", Buffer.byteLength(bodyData));
+            // stream the content
+            proxyReq.write(bodyData);
+          }
         },
         /*onProxyRes: (proxyRes, req, res) => {
           // Send a response to the client here if needed
